ff57958458868c968a276366814078d6e2a9d5bc
Lucas Brugnago da Maia <l.brugnago@hotmail.com>
2022-03-02 13:56:21 -0300

    Adicionando arquivos e editando
    Adicionado arquivo html e fonts
    
    Editado css e js

--- js/flappy.js
+++ js/flappy.js
@@ -1,1 +1,14 @@
-// ...
+function novoElemento(tagName, className) {
+    const elem = document.createElement(tagName)
+    Element.className = className
+    return elem
+}
+
+function Barreira(reserva = false) {
+    this.elemento = novoElemento('div', 'barreira')
+
+    const borda = novoElemento('div', 'borda')
+    const corpo = novoElemento('div', 'corpo')
+    this.elemento.appendChild(reserva ? corpo : borda)
+    this.elemento.appendChild(reserva ? borda : corpo)
+}
