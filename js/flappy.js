09c2ed16af10d558282c999874622454838d59c1
Lucas Brugnago da Maia <l.brugnago@hotmail.com>
2022-03-03 13:52:49 -0300

    Update flappy.js

--- js/flappy.js
+++ js/flappy.js
@@ -11,4 +11,26 @@ function Barreira(reserva = false) {
     const corpo = novoElemento('div', 'corpo')
     this.elemento.appendChild(reserva ? corpo : borda)
     this.elemento.appendChild(reserva ? borda : corpo)
+    this.setAltura = altura => corpo.style.height = `${altura}px`
+}
+
+/*const b = new Barreira(true)
+b.setAltura(300)
+document.querySelector('[wm-flappy]').appendChild(b.elemento)*/
+
+function ParDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras')
+
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.superior.setAltura(alturaInferior)
+    }
 }
